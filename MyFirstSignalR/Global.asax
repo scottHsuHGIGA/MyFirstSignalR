6970c07f1cf690d2de313a252530bee2d840c37d
jwsdushscca <scott.hsu@hgiga.com>
2021-11-19 17:41:41 +0800

    first commit

---
+++ MyFirstSignalR/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyFirstSignalR.MvcApplication" Language="C#" %>
---
+++ MyFirstSignalR/SignalR/Hubs/ChatHub.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MyFirstSignalR.SignalR.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            // Call the addNewMessageToPage method to update clients.
+            Clients.All.addNewMessageToPage(name, message);
+            Clients.All.Test("隨便");
+            JoinRoom("TestRoom");
+            Clients.Group("TestRoom").addChatMessage(name, "隨便");
+        }
+
+        public async Task JoinRoom(string roomName)
+        {
+            await Groups.Add(Context.ConnectionId, roomName);
+            Clients.Group(roomName).addChatMessage("system",Context.User.Identity.Name + " joined.");
+        }
+
+        public Task LeaveRoom(string roomName)
+        {
+            return Groups.Remove(Context.ConnectionId, roomName);
+        }
+    }
+}
